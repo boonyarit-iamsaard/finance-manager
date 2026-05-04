3a03f0394a7c38abca26ce6620adb12ad0ea9cd7
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2026-05-04 09:10:24 +0700

    build: initialize project

---
+++ apps/server/src/index.ts
@@ -0,0 +1,29 @@
+import { auth } from "@finance-manager/auth";
+import { env } from "@finance-manager/env/server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: env.CORS_ORIGIN,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+export default {
+  port: env.PORT,
+  fetch: app.fetch,
+};
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,36 @@
+import { createDb } from "@finance-manager/db";
+import * as schema from "@finance-manager/db/schema/auth";
+import { env } from "@finance-manager/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+      schema: schema,
+      usePlural: true,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      database: {
+        generateId: "uuid",
+      },
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
